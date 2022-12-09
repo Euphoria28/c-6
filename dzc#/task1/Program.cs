ad65b8de529d6671de443a0c3f3fe08e6adfdd02
Euphoria <polunine28@gmail.com>
2022-12-09 16:02:36 +1100

    task1

---
+++ dzc#/task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Размер > ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandom(numbers);
+System.Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine($"{numbers.Length}, {count} - четные");
+
+void FillArrayRandom(int[] numbers)
+{
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        numbers[j] = new Random().Next(10,100);
+
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[");
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        Console.Write(numbers[j] + "");
+    }
+    Console.Write("]");
+    System.Console.WriteLine();
+}
